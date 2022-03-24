c80a86d061693fb2f3ffd08c58fbe1f56290940d
Derek McNealy <derek.mcnealy91@gmail.com>
2022-03-24 18:47:07 -0400

    added payment forms

--- src/script.js
+++ src/script.js
@@ -1,39 +1,40 @@
+document.querySelector("#coffeeDrpDown").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#coffeePage").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+});
 
-document.querySelector('#coffeeDrpDown').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.querySelector('#coffeePage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
+document.querySelector("#hcDrpDown").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#hotChocolatePage").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+});
 
-document.querySelector('#hcDrpDown').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.querySelector('#hotChocolatePage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
+document.querySelector("#teaDrpDown").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#teaPage").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+});
 
-document.querySelector('#teaDrpDown').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.querySelector('#teaPage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
+document.querySelector("#muffinDrpDown").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#muffinsPage").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+});
 
-document.querySelector('#muffinDrpDown').addEventListener("click", (event) => { 
+document
+  .querySelector("#sandwichDrpDown")
+  .addEventListener("click", (event) => {
     event.preventDefault();
-    document.querySelector('#muffinsPage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
+    document.querySelector("#sandwichPage").style.display = "block";
+    document.querySelector(".mainPage").style.display = "none";
+  });
 
-document.querySelector('#sandwichDrpDown').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.querySelector('#sandwichPage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
-
-document.querySelector('#bagelDrpDown').addEventListener("click", (event) => { 
-    event.preventDefault();
-    document.querySelector('#bagelPage').style.display = "block";
-    document.querySelector('.mainPage').style.display = "none";
-})
+document.querySelector("#bagelDrpDown").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector("#bagelPage").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+});
 
 function showCartContainer(e) {
   var containers = document.querySelectorAll(".container");
@@ -62,4 +63,47 @@ document.querySelector("#myquantityform").addEventListener("submit", (e) => {
   }`;
   e.preventDefault();
 });
+// when you click checkout in cart takes you to payment method radio button forms
+document.querySelector(".checkout").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.querySelector(".payOptions").style.display = "block";
+  document.querySelector(".mainPage").style.display = "none";
+  document.querySelector(".Cart-Items").style.display = "none";
+  document.querySelector(".shoppingCart").style.display = "none";
+});
+// *****when you choose the cash radio button it takes you to cash method form***
 
+const btnPm = document.querySelector(".paymentMethodSubmit");
+const payMethodBtns = document.querySelectorAll(
+  'input[name = "paymentMethod"]'
+);
+btnPm.addEventListener("click", (event) => {
+  event.preventDefault();
+  for (const payMethodBtn of payMethodBtns) {
+    if (payMethodBtn.checked) {
+      console.log(payMethodBtn.id);
+      if (payMethodBtn.id === "cashMethod") {
+        document.querySelector(".cashOption").style.display = "block";
+        document.querySelector(".payOptions").style.display = "none";
+        document.querySelector(".mainPage").style.display = "none";
+        document.querySelector(".Cart-Items").style.display = "none";
+      } else if (payMethodBtn.id === "cardMethod") {
+        document.querySelector(".cardOption").style.display = "block";
+        document.querySelector(".payOptions").style.display = "none";
+        document.querySelector(".mainPage").style.display = "none";
+        document.querySelector(".Cart-Items").style.display = "none";
+      }
+      break;
+    }
+  }
+});
+// getting data from the cash and card forms
+// cash form
+document.querySelector(".cashSubmit").addEventListener("click", (event) => {
+  event.preventDefault();
+  const form = document.querySelector(".cashForm");
+  const Data = new FormData(form);
+  let amountGiven = data.get("given");
+  console.log(amountGiven);
+  console.log(Data);
+});
